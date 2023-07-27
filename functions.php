bf1ef37a8de27a10066daa53dbdd7110f728511c
aminulmhub <aminulmhub@gmail.com>
2023-07-27 11:30:43 +0600

    Header logo and short bio added with wp_customize

--- functions.php
+++ functions.php
@@ -1,18 +1,60 @@
-<!-- Load CSS -->
 <?php
-function load__css()
+/* 
+* Theme Functions
+*/
+
+// theme title
+add_theme_support('title-tag');
+
+// theme style and js call
+function theme_css_js()
 {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css', array(), false, 'all');
-    wp_enqueue_style('bootstrap');
+    // default css
+    wp_enqueue_style('style', get_stylesheet_uri());
+    // bootstrap css
+    wp_register_style('bootstrap_style', get_template_directory_uri() . '/assets/vendors/bootstrap/css/bootstrap.min.css', array(), '5.3.0');
+    wp_enqueue_style('bootstrap_style');
+    // icon
+    wp_register_style('fontawesome_icon', get_template_directory_uri() . '/assets/icons/fontawesome-6.3.0/css/all.min.css', array(), '6.3.0');
+    wp_enqueue_style('fontawesome_icon');
+    // theme css
+    wp_register_style('main_style', get_template_directory_uri() . "/assets/css/main.css", filemtime(get_template_directory() . "/assets/css/main.css"), '1.0.0', 'all');
+    wp_enqueue_style('main_style');
+
+    // default jquery
+    wp_enqueue_script('jquery');
+    // bootstrap js
+    wp_enqueue_script('bootstrap_js', get_template_directory_uri() . "/assets/vendors/bootstrap/js/bootstrap.bundle.min.js", array(), "5.3.0", 'true');
+    wp_enqueue_script('main_js', get_template_directory_uri() . "/assets/js/main.js", array(), "1.0.0", 'true');
 }
-add_action('wp_enqueue_scripts', 'load__css');
+
+add_action("wp_enqueue_scripts", 'theme_css_js');
 
 
-
-// Load JS
-function load__js()
-{
-    wp_register_script('bootstrapjs', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.bundle.min.js', array(), false, true);
-    wp_enqueue_script('bootstrapjs');
+// theme functions
+function custom_theme_customizer($wp_customize){
+    $wp_customize->add_section('header_area', array(
+        'title' => __('Header', 'aminulislam'),
+        'description' => 'Change header content'
+    ));
+    // section setting
+    $wp_customize->add_setting('header_logo', array(
+        'default' => get_bloginfo('template_directory') . '/assets/images/logo/logo-dark.png'
+    ));
+    $wp_customize->add_setting('short_bio', array(
+        'default' => 'Portfolio short description'
+    ));
+    // section control
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'header_logo', array(
+        'label' => 'Upload logo',
+        'section' => 'header_area',
+        'setting' => 'header_logo'
+    )));
+    $wp_customize->add_control('short_bio', array(
+        'label' => 'Short bio',
+        'section' => 'header_area',
+        'setting' => 'short_bio'
+    ));
 }
-add_action('wp_enqueue_scripts', 'load__js');
+
+add_action('customize_register', 'custom_theme_customizer');
--- header.php
+++ header.php
@@ -1,12 +1,100 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Start Header -->
+<header class="template-header">
+        <div class="template-navigation">
+            <div class="nav-overlay"></div>
+            <div class="container-fluid p-0">
+                <div class="template-menu">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Custom Theme</title>
+                    <!-- Template Logo -->
+                    <div class="template-branding">
+                        <a href="index.html" class="template-logo">
+                            <img src="<?php echo get_theme_mod('header_logo'); ?>" alt="Site Logo" />
+                        </a>
+                    </div>
 
-        <?php wp_head() ?>
-    </head>
+                    <div class="nav-menu">
+                        <div class="d-xl-none">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <!-- sidebar logo -->
+                                <div class="sidebar-logo">
+                                    <a href="index.html" class="template-logo">
+                                        <img src="<?php echo get_theme_mod('header_logo') ?>" alt="Site Logo" />
+                                    </a>
+                                </div>
 
-    <body>
+                                <!-- Navbar Close -->
+                                <div class="navbar-close shade_box btn_primary rounded-circle">
+                                    <i class="fa-solid fa-angles-left"></i>
+                                </div>
+                            </div>
+                            <p class="m-0 border-bottom my-4 pb-4">
+                                <?php echo get_theme_mod('short_bio'); ?>
+                            </p>
+                        </div>
+
+                        <!-- Nav Menu -->
+                        <nav class="main-menu">
+                            <ul>
+                                <li class="menu-item">
+                                    <a href="#home">Home</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#features">Features</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#portfolio">Portfolio</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#resume">Resume</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#clients">Clients</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#pricing">Pricing</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#blog">Blog</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#contactID">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <!-- Social Link -->
+                        <div class="d-xl-none social_links_wrap">
+                            <span class="sub_title">Find with me</span>
+                            <div class="social_links">
+                                <a href="#" class="shade_box btn_primary">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                                <a href="#" class="shade_box btn_primary">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                                <a href="#" class="shade_box btn_primary">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="nav-right">
+                        <div class="d-none d-xl-block">
+                            <a href="#" class="btn_primary shade_box btn_cta">Buy Now</a>
+                        </div>
+                        <ul class="d-flex align-items-center m-0">
+                            <li class="nav-toggle-btn">
+                                <div class="navbar-toggler">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- End Header -->
